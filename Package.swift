93d8d099489be56ea390d31979912543f151c07a
John Sundell <john@sundell.co>
2017-04-09 12:42:13 +0200

    Lowercase “g” in name to match “Xcode”

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "XGen",
+    name: "Xgen",
     dependencies: [
         .Package(url: "https://github.com/johnsundell/files.git", majorVersion: 1)
     ]
