f463b9de5a1e3d003ecb2f2e9e8bbccb23e8d7c9
John Sundell <john@sundell.co>
2017-04-09 19:59:28 +0200

    Use SSH to clone Files

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Xgen",
     dependencies: [
-        .Package(url: "https://github.com/johnsundell/files.git", majorVersion: 1)
+        .Package(url: "git@github.com:johnsundell/files.git", majorVersion: 1)
     ]
 )
