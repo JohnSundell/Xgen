ef1ab9456caacbd3ae9592ae40c4a744805ca4da
John Sundell <john@sundell.co>
2017-05-15 20:31:03 +0200

    Use HTTPS when cloning Files

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Xgen",
     dependencies: [
-        .Package(url: "git@github.com:johnsundell/files.git", majorVersion: 1)
+        .Package(url: "https://github.com/JohnSundell/Files.git", majorVersion: 1)
     ]
 )
