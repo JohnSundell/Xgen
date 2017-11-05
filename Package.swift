58d8691802ee57f3d63e93f40493630e8beba29c
John Sundell <john@sundell.co>
2017-11-05 21:07:04 +0100

    Bump Files to 2.0.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Xgen", targets: ["Xgen"])
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/Files.git", from: "1.0.0")
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "2.0.0"),
     ],
     targets: [
         .target(
